da5f92d1e662b94a594c920773ee47a4a6599930
Denys <4irelly@gmail.com>
2023-09-04 11:07:11 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let storeName = "Pappapizza";
+
+const storeDescription = {
+    budget: 10000,
+    employees: [John, Kolia, Denys],
+    products: {
+        apple: 20,
+        juce: 15
+    },
+    open: true
+}
